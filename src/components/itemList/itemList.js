73b2624870f0bc4cb66019c6d3ee6225ae563d04
u.dukenbay <urziya.dukenbay@gmail.com>
2021-01-03 17:06:36 +0600

    merge conflicts

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, {useState, useEffect} from 'react';
 import Spinner from '../spinner/';
 import './itemList.css';
@@ -22,6 +23,22 @@ function ItemList({getData, onItemSelected, renderItem}) {
 
             const label = renderItem(item);
 
+=======
+import React, {Component} from 'react';
+import Spinner from '../spinner/';
+import './itemList.css';
+import gotService from '../../services/gotService';
+
+class ItemList extends Component {
+
+    renderItems(arr) {
+
+        return arr.map((item) => {
+            const {id} = item
+
+            const label = this.props.renderItem(item);
+
+>>>>>>> 661d65c3b05173a588c7be10adfe277f51ade9ca
             return (
                 <li
                     key={id}
@@ -38,6 +55,7 @@ function ItemList({getData, onItemSelected, renderItem}) {
         return <Spinner/>
     }
 
+<<<<<<< HEAD
     const items = renderItems(itemList);
 
     return (
@@ -48,6 +66,12 @@ function ItemList({getData, onItemSelected, renderItem}) {
     );
 }
 
+=======
+    render() {
+
+        const {data} = this.props;
+        const items = this.renderItems(data);
+>>>>>>> 661d65c3b05173a588c7be10adfe277f51ade9ca
 
 // ItemList.defaultProps = {
 //     onItemSelected: () => {}
@@ -63,6 +87,54 @@ function ItemList({getData, onItemSelected, renderItem}) {
 // f(1)(2);
 
 
+<<<<<<< HEAD
 
 // const {getAllCharacters} = new gotService();
 export default ItemList;
+=======
+
+ItemList.defaultProps = {
+    onItemSelected: () => {}
+}
+
+// const f = (a) => {
+//     console.log(a);
+//     return (b) => {
+//         console.log(a + b);
+//     }
+// }
+
+// f(1)(2);
+
+const withData = (View, getData) => {
+    return class extends Component {
+
+        state = {
+            data: null
+        }
+    
+        componentDidMount() {
+    
+            getData()
+                .then( (data) => {
+                    this.setState({
+                        data
+                    })
+                })
+        }
+
+        render() {
+            const {data} = this.state;
+
+            if(!data) {
+                return <Spinner/>
+            }
+
+            return <View {...this.props} data={data} />
+        }
+    }
+}
+
+const {getAllCharacters} = new gotService();
+export default withData(ItemList, getAllCharacters);
+>>>>>>> 661d65c3b05173a588c7be10adfe277f51ade9ca
